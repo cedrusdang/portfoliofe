388291ed1095c11b3020af84d6d59faf2b800527
Cedrus Dang <dtt161094@gmail.com>
2025-07-03 04:51:47 +0800

    add header and content+background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
+        <img src='Logo.svg' className="App-logo" alt="logo" />
+        <nav className='App-menu'>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </nav>
+      </div>
+      
+      <div className='Banner-Background'>
+        <div className='Banner'>
+          <div className='Banner-content'>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <button className='Reserve-button'>Reserve a Table</button>
+          </div>
+          <img src='restaurantfood.jpg' className='Banner-image' alt="Little Lemon"/>
+        </div>
+      </div>
 
     </div>
   );
